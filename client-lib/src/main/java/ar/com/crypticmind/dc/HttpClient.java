203fd2255dbf71fcaa632ee2c86622b7ac25d234
Carlos Ferreyra <cferreyra@despegar.com>
2017-10-04 19:03:42 -0300

    WIP

---
+++ client-lib/src/main/java/ar/com/crypticmind/dc/HttpClient.java
@@ -0,0 +1,45 @@
+package ar.com.crypticmind.dc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class HttpClient {
+
+    public static <T> T doGET(URL url, Function<byte[], T> onSuccess) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        try (InputStream is = conn.getInputStream()) {
+            byte[] buffer = read(is, conn.getContentLength());
+            return onSuccess.apply(buffer);
+        } catch (IOException e) {
+            try (InputStream es = conn.getErrorStream()) {
+                byte[] buffer = read(es, conn.getContentLength());
+                int statusCode = conn.getResponseCode();
+                throw new RuntimeException("GET " + url + " failed → HTTP " + statusCode + ": " + readString.apply(buffer));
+            }
+        }
+    }
+
+    public static final Consumer<byte[]> ignore = buffer -> {};
+
+    public static Function<byte[], String> readString = buffer -> {
+        try {
+            return new String(buffer, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Could not read UTF-8 text response", e);
+        }
+    };
+
+    private static byte[] read(InputStream is, int length) throws IOException {
+        byte[] buffer = new byte[length];
+        int pos = 0;
+        int b;
+        while ((b = is.read()) > 0)
+            buffer[pos++] = (byte) b;
+        return buffer;
+    }
+}
